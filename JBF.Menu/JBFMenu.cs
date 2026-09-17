af1a17b57e9c74988fa72bded5771f40bf816997
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-17 19:16:34 +0300

    Move JBF menu rendering to Panorama HUD

--- JBF.Menu/JBFMenu.cs
+++ JBF.Menu/JBFMenu.cs
@@ -7,17 +7,28 @@ namespace JBF.Menu;
 
 public sealed class JBFMenu : BasePlugin
 {
-    private readonly MenuService _menuService = new();
+    private MenuService? _menuService;
 
     public override string ModuleName => "JBF Menu";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "2.0.0";
     public override string ModuleAuthor => "Mell";
 
     public override void Load(bool hotReload)
     {
+        _menuService = new MenuService(
+            "panorama/layout/custom_game/jbf_menu.xml",
+            message => Logger.LogInformation("{Message}", message));
+
+        _menuService.Start(this, hotReload);
+
         Capabilities.RegisterPluginCapability(MenuCapability.Api, () => _menuService);
         RegisterListener<Listeners.OnPlayerButtonsChanged>(_menuService.HandleButtonsChanged);
         RegisterListener<Listeners.OnClientDisconnect>(_menuService.HandleClientDisconnect);
-        RegisterListener<Listeners.OnTick>(_menuService.Render);
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        _menuService?.Stop(this);
+        _menuService = null;
     }
 }
