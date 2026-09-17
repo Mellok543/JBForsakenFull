2a0304fef7c8b7a42c3301e1eace04fc3b273493
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-17 15:59:36 +0300

    Expose warden claimed event for achievements

--- JBF.Api/IWardenApi.cs
+++ JBF.Api/IWardenApi.cs
@@ -6,6 +6,8 @@ public interface IWardenApi
 {
     CCSPlayerController? Warden { get; }
 
+    event Action<CCSPlayerController>? WardenClaimed;
+
     bool IsWarden(CCSPlayerController player);
 
     bool TryResign(CCSPlayerController player);
