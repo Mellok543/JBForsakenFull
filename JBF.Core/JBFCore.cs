e705420b8b4414a126eda38f5af188876caa586f
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-17 12:08:59 +0300

    Register shared player effects service

--- JBF.Core/JBFCore.cs
+++ JBF.Core/JBFCore.cs
@@ -11,24 +11,29 @@ public sealed class JBFCore : BasePlugin
 {
     private readonly RoundService _roundService = new();
     private readonly IJailbreakApi _api;
+    private readonly PlayerEffectsService _effects;
 
     public JBFCore()
     {
         _api = new JailbreakApi(_roundService);
+        _effects = new PlayerEffectsService(this);
     }
 
     public override string ModuleName => "JBF Core";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.1.0";
     public override string ModuleAuthor => "Mell";
 
     public override void Load(bool hotReload)
     {
         Capabilities.RegisterPluginCapability(JailbreakCapability.Api, () => _api);
+        Capabilities.RegisterPluginCapability(PlayerEffectsCapability.Api, () => _effects);
+        RegisterListener<Listeners.OnClientDisconnect>(OnClientDisconnect);
     }
 
     [GameEventHandler]
     public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
     {
+        _effects.ClearAll();
         _roundService.RoundStart();
         return HookResult.Continue;
     }
@@ -36,7 +41,20 @@ public sealed class JBFCore : BasePlugin
     [GameEventHandler]
     public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
     {
+        _effects.ClearAll();
         _roundService.RoundEnd();
         return HookResult.Continue;
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        _effects.ClearAll();
+    }
+
+    private void OnClientDisconnect(int playerSlot)
+    {
+        var player = CounterStrikeSharp.API.Utilities.GetPlayerFromSlot(playerSlot);
+        if (player is not null)
+            _effects.Clear(player);
     }
 }
