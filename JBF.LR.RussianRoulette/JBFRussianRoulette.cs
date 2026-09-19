80b7ff5d0c74ea486d896ba3cd11b4915be506b8
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-19 20:42:13 +0300

    Keep JBF.LR.RussianRoulette registered after LR core reload

--- JBF.LR.RussianRoulette/JBFRussianRoulette.cs
+++ JBF.LR.RussianRoulette/JBFRussianRoulette.cs
@@ -1,5 +1,6 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Modules.Entities.Constants;
 using CounterStrikeSharp.API.Modules.Utils;
 using System.Drawing;
@@ -10,6 +11,7 @@ namespace JBF.LR.RussianRoulette;
 public sealed class JBFRussianRoulette : BasePlugin
 {
     private readonly RussianRouletteGame _game = new();
+    private ILrApi? _registeredApi;
     private IDisposable? _registration;
 
     public override string ModuleName => "JBF LR: Russian Roulette";
@@ -18,19 +20,34 @@ public sealed class JBFRussianRoulette : BasePlugin
 
     public override void Load(bool hotReload)
     {
+        AddTimer(2.0f, EnsureRegistration, TimerFlags.REPEAT | TimerFlags.STOP_ON_MAPCHANGE);
         RegisterListener<Listeners.OnTick>(_game.Tick);
     }
 
     public override void OnAllPluginsLoaded(bool hotReload)
     {
-        var lr = LrCapability.Api.Get();
-        if (lr is not null) _registration = lr.RegisterGame(_game);
+        EnsureRegistration();
     }
 
     public override void Unload(bool hotReload)
     {
         _registration?.Dispose();
+        _registration = null;
+        _registeredApi = null;
         _game.Stop();
+    }
+    private void EnsureRegistration()
+    {
+        var current = LrCapability.Api.Get();
+        if (ReferenceEquals(current, _registeredApi))
+            return;
+
+        _registration?.Dispose();
+        _registration = null;
+        _registeredApi = current;
+
+        if (current is not null)
+            _registration = current.RegisterGame(_game);
     }
 }
 
