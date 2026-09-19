413df45828b24c4456913e2bbb8d0a7adbd1b66f
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-19 20:42:00 +0300

    Keep JBF.LR.KnifeDuel registered after LR core reload

--- JBF.LR.KnifeDuel/JBFKnifeDuel.cs
+++ JBF.LR.KnifeDuel/JBFKnifeDuel.cs
@@ -1,5 +1,6 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Core.Capabilities;
 using CounterStrikeSharp.API.Modules.Entities.Constants;
 using JBF.Api;
@@ -9,6 +10,7 @@ namespace JBF.LR.KnifeDuel;
 public sealed class JBFKnifeDuel : BasePlugin
 {
     private readonly KnifeDuelGroup _game = new();
+    private ILrApi? _registeredApi;
     private IDisposable? _registration;
 
     public override string ModuleName => "JBF LR: Knife Duel";
@@ -17,16 +19,34 @@ public sealed class JBFKnifeDuel : BasePlugin
 
     public override void Load(bool hotReload)
     {
+        AddTimer(2.0f, EnsureRegistration, TimerFlags.REPEAT | TimerFlags.STOP_ON_MAPCHANGE);
         RegisterListener<Listeners.OnPlayerButtonsChanged>(_game.OnButtonsChanged);
     }
 
     public override void OnAllPluginsLoaded(bool hotReload)
     {
-        var lr = LrCapability.Api.Get();
-        if (lr is not null) _registration = lr.RegisterGame(_game);
+        EnsureRegistration();
     }
 
-    public override void Unload(bool hotReload) => _registration?.Dispose();
+    public override void Unload(bool hotReload)
+    {
+        _registration?.Dispose();
+        _registration = null;
+        _registeredApi = null;
+    }
+    private void EnsureRegistration()
+    {
+        var current = LrCapability.Api.Get();
+        if (ReferenceEquals(current, _registeredApi))
+            return;
+
+        _registration?.Dispose();
+        _registration = null;
+        _registeredApi = current;
+
+        if (current is not null)
+            _registration = current.RegisterGame(_game);
+    }
 }
 
 internal sealed class KnifeDuelGroup : ILrGame, ILrGameVariants
