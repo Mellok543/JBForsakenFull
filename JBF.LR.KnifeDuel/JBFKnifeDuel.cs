dc84fbbcc45b4141c2ff47862176043527feb77a
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-20 15:50:21 +0300

    Allow LR game module to run without LR core

--- JBF.LR.KnifeDuel/JBFKnifeDuel.cs
+++ JBF.LR.KnifeDuel/JBFKnifeDuel.cs
@@ -36,7 +36,7 @@ public sealed class JBFKnifeDuel : BasePlugin
     }
     private void EnsureRegistration()
     {
-        var current = LrCapability.Api.Get();
+        var current = LrCapability.Api.GetOptional();
         if (ReferenceEquals(current, _registeredApi))
             return;
 
