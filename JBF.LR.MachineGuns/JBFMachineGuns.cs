2d7fabf55f581a3a596a60e42e337f9deb609a07
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-20 15:50:23 +0300

    Allow LR game module to run without LR core

--- JBF.LR.MachineGuns/JBFMachineGuns.cs
+++ JBF.LR.MachineGuns/JBFMachineGuns.cs
@@ -29,7 +29,7 @@ public sealed class JBFMachineGuns : BasePlugin
     }
     private void EnsureRegistration()
     {
-        var current = LrCapability.Api.Get();
+        var current = LrCapability.Api.GetOptional();
         if (ReferenceEquals(current, _registeredApi))
             return;
 
