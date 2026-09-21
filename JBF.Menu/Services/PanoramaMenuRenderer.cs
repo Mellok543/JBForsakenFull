f133d583f96c179cf44a0a277b59496fa9c7eb4e
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-21 22:28:39 +0300

    Clear mute HUD with menu cleanup

--- JBF.Menu/Services/PanoramaMenuRenderer.cs
+++ JBF.Menu/Services/PanoramaMenuRenderer.cs
@@ -93,6 +93,7 @@ internal sealed class PanoramaMenuRenderer
                      "jbf_menu_root",
                      "jbf_question_root",
                      "jbf_mapvote_root",
+                     "jbf_mute_root",
                      "jbf_notify_root",
                      "jbf_announce_root",
                      "jbf_round_root",
