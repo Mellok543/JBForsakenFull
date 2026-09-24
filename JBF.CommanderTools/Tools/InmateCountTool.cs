6d5e257c1cdf4a7c34a39a7235ae02f4336f94f8
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-24 17:16:31 +0300

    Report inmates inside commander count circle

--- JBF.CommanderTools/Tools/InmateCountTool.cs
+++ JBF.CommanderTools/Tools/InmateCountTool.cs
@@ -1,5 +1,7 @@
+using System.Drawing;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Modules.Utils;
 using JBF.Api;
 using JBF.CommanderTools.Extensions;
@@ -8,7 +10,18 @@ namespace JBF.CommanderTools.Tools;
 
 internal sealed class InmateCountTool : ICommanderTool
 {
-    private const float FarDistance = 500.0f;
+    private const float CircleRadius = 500.0f;
+    private const float CircleLifetimeSeconds = 5.0f;
+    private const int CircleSegments = 28;
+    private const float BeamWidth = 3.0f;
+
+    private readonly BasePlugin _plugin;
+    private readonly List<CBeam> _beams = [];
+
+    public InmateCountTool(BasePlugin plugin)
+    {
+        _plugin = plugin;
+    }
 
     public string Id => "inmate-count";
     public string Text => "Посчитать заключённых";
@@ -18,27 +31,120 @@ internal sealed class InmateCountTool : ICommanderTool
     {
         var commanderPosition = commander.PlayerPawn.Value?.AbsOrigin;
         if (commanderPosition is null)
+            return;
+
+        ClearCircle();
+        DrawCircle(commanderPosition);
+
+        commander.PrintToChat(JailbreakChat.Format(
+            $"Круг подсчёта активен {CircleLifetimeSeconds:0} сек. Заключённые должны находиться внутри круга."));
+
+        _plugin.AddTimer(
+            CircleLifetimeSeconds,
+            () => FinishCount(commander),
+            TimerFlags.STOP_ON_MAPCHANGE);
+    }
+
+    private void FinishCount(CCSPlayerController commander)
+    {
+        ClearCircle();
+
+        if (!commander.IsUsable() || !commander.PawnIsAlive)
+            return;
+
+        var center = commander.PlayerPawn.Value?.AbsOrigin;
+        if (center is null)
+            return;
+
+        var aliveInmates = Utilities.GetPlayers()
+            .Where(player =>
+                player.IsUsable() &&
+                player.PawnIsAlive &&
+                player.Team == CsTeam.Terrorist)
+            .ToArray();
+
+        var inside = new List<CCSPlayerController>();
+        var outside = new List<CCSPlayerController>();
+
+        foreach (var inmate in aliveInmates)
         {
+            var position = inmate.PlayerPawn.Value?.AbsOrigin;
+            if (position is not null && Distance2D(center, position) <= CircleRadius)
+                inside.Add(inmate);
+            else
+                outside.Add(inmate);
+        }
+
+        Server.PrintToChatAll(JailbreakChat.Format(
+            $"В круге КМД: {inside.Count}/{aliveInmates.Length} заключённых."));
+
+        if (outside.Count == 0)
+        {
+            Server.PrintToChatAll(JailbreakChat.Format("Все живые заключённые находятся внутри круга."));
             return;
         }
 
-        var farInmates = Utilities.GetPlayers()
-            .Where(player => player.IsUsable() && player.PawnIsAlive && player.Team == CsTeam.Terrorist)
-            .Where(player => player.PlayerPawn.Value?.AbsOrigin is { } position &&
-                             Distance(commanderPosition, position) > FarDistance)
-            .Select(player => player.PlayerName)
-            .ToArray();
-
-        commander.PrintToChat(JailbreakChat.Format(farInmates.Length == 0
-            ? "Все заключённые рядом с командиром."
-            : $"Заключённые далеко: {string.Join(", ", farInmates)}"));
+        Server.PrintToChatAll(JailbreakChat.Format(
+            $"Вне круга: {string.Join(", ", outside.Select(player => player.PlayerName))}."));
     }
 
-    private static float Distance(Vector first, Vector second)
+    private void DrawCircle(Vector center)
+    {
+        var points = new Vector[CircleSegments];
+
+        for (var i = 0; i < CircleSegments; i++)
+        {
+            var angle = (float)i / CircleSegments * MathF.PI * 2.0f;
+            points[i] = new Vector(
+                center.X + CircleRadius * MathF.Cos(angle),
+                center.Y + CircleRadius * MathF.Sin(angle),
+                center.Z + 5.0f);
+        }
+
+        for (var i = 0; i < CircleSegments; i++)
+            DrawBeam(points[i], points[(i + 1) % CircleSegments]);
+    }
+
+    private void DrawBeam(Vector start, Vector end)
+    {
+        var beam = Utilities.CreateEntityByName<CBeam>("beam");
+        if (beam is null || !beam.IsValid)
+            return;
+
+        beam.Width = BeamWidth;
+        beam.Render = Color.LimeGreen;
+        beam.DispatchSpawn();
+        beam.Teleport(start, new QAngle(), new Vector());
+
+        beam.EndPos.X = end.X;
+        beam.EndPos.Y = end.Y;
+        beam.EndPos.Z = end.Z;
+        Utilities.SetStateChanged(beam, "CBeam", "m_vecEndPos");
+
+        _beams.Add(beam);
+    }
+
+    private void ClearCircle()
+    {
+        foreach (var beam in _beams.ToArray())
+        {
+            try
+            {
+                if (beam.IsValid)
+                    beam.Remove();
+            }
+            catch
+            {
+            }
+        }
+
+        _beams.Clear();
+    }
+
+    private static float Distance2D(Vector first, Vector second)
     {
         var x = second.X - first.X;
         var y = second.Y - first.Y;
-        var z = second.Z - first.Z;
-        return MathF.Sqrt(x * x + y * y + z * z);
+        return MathF.Sqrt(x * x + y * y);
     }
 }
