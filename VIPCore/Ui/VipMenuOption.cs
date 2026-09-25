6d806d26af3f05d7d9040429969f81bca40ea6af
Mellok543 <73647025+Mellok543@users.noreply.github.com>
2026-09-25 11:51:01 +0300

    Remove standalone VIP UI resources

--- VIPCore/Ui/VipMenuOption.cs
+++
@@ -1,9 +0,0 @@
-using CounterStrikeSharp.API.Core;
-
-namespace VIPCore.Ui;
-
-internal sealed record VipMenuOption(
-    string Text,
-    Action<CCSPlayerController> OnSelect,
-    bool IsDisabled = false,
-    string? DisabledReason = null);
